c1fbbc00f9a2a1b0fb61b787f5d3214b40b591a9
martaqh <marta.k.herma@gmail.com>
2023-06-27 12:36:04 +0200

    Init project

---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: () => import('../pages/HomePage.vue')
+//     },
+//     {
+//       path: '/dashboard',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../pages/BasePage.vue'),
+//       props: true
+//     }
+//   ]
+// })
+
+// export default router
