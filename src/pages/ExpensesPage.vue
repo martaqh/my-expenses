2a201dc743c864a333443a66c8edd9742b0759cf
martaqh <marta.k.herma@gmail.com>
2023-06-29 17:43:27 +0200

    Add filtering by date & category & name

--- src/pages/ExpensesPage.vue
+++ src/pages/ExpensesPage.vue
@@ -3,6 +3,7 @@ import { computed, ref } from 'vue';
 import BasePage from '@/pages/BasePage.vue';
 import ExpenseItem from '@/components/ExpenseItem.vue';
 import SummaryView from '@/components/SummaryView.vue';
+import Datepicker from 'vue3-datepicker';
 
 export type Category = 'food' | 'car';
 
@@ -122,9 +123,35 @@ const totalsPerCategory = computed(() => {
   return categoriesAndTotals;
 });
 
-const paginationLength = computed(() => {
-  console.log(Math.ceil(expenses.length / 5));
-  return Math.ceil(expenses.length / 5);
+const filteredExpenses = computed(() => {
+  if (
+    !selectedStartDate.value &&
+    !selectedEndDate.value &&
+    !selectedCategory.value &&
+    !selectedName.value
+  ) {
+    return expenses;
+  } else {
+    const startTimestamp = selectedStartDate.value ? selectedStartDate.value.getTime() : 0;
+    const endTimestamp = selectedEndDate.value
+      ? selectedEndDate.value.getTime()
+      : Number.MAX_SAFE_INTEGER;
+    const selectedCategoryValue = selectedCategory.value;
+    const selectedNameValue = selectedName.value.toLowerCase();
+
+    return expenses.filter((item) => {
+      const itemTimestamp = new Date(item.date).getTime();
+      const itemCategory = item.category;
+      const itemName = item.name.toLowerCase();
+
+      return (
+        itemTimestamp >= startTimestamp &&
+        itemTimestamp <= endTimestamp &&
+        (!selectedCategoryValue || itemCategory === selectedCategoryValue) &&
+        (!selectedNameValue || itemName.includes(selectedNameValue))
+      );
+    });
+  }
 });
 
 const page = ref(1);
@@ -133,11 +160,24 @@ const itemsPerPage = 5;
 const paginatedExpenseItems = computed(() => {
   const startIndex = (page.value - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  return expenses.slice(startIndex, endIndex);
+  return filteredExpenses.value.slice(startIndex, endIndex);
 });
 
 const totalPages = computed(() => {
   return Math.ceil(expenses.length / itemsPerPage);
+});
+
+const selectedStartDate = ref<Date>();
+const selectedEndDate = ref<Date>();
+
+const selectedCategory = ref<Category | ''>('');
+const selectedName = ref('');
+
+const inputStyle = computed(() => {
+  return {
+    border: '1px solid grey',
+    padding: '4px 8px',
+  };
 });
 </script>
 
@@ -145,6 +185,34 @@ const totalPages = computed(() => {
   <BasePage title="Expenses">
     <div class="expenses-page">
       <div class="expenses-page__items-list">
+        <div class="expenses-page__filters">
+          <div class="expenses-page__filters--date">
+            <Datepicker
+              v-model="selectedStartDate"
+              :lower-limit="new Date('2022-07-16')"
+              :upper-limit="new Date('2022-09-16')"
+              placeholder="Choose start date"
+              :style="inputStyle"
+              typeable
+            />
+            <Datepicker
+              v-model="selectedEndDate"
+              :lower-limit="new Date('2022-07-16')"
+              :upper-limit="new Date('2022-09-16')"
+              placeholder="Choose end date"
+              :style="inputStyle"
+              typeable
+            />
+          </div>
+          <div class="expenses-page__filters--category">
+            <v-btn-toggle v-model="selectedCategory" mandatory variant="outlined" divided>
+              <v-btn @click="selectedCategory = ''">All</v-btn>
+              <v-btn @click="selectedCategory = 'car'">Car</v-btn>
+              <v-btn @click="selectedCategory = 'food'">Food</v-btn>
+            </v-btn-toggle>
+          </div>
+          <v-autocomplete v-model="selectedName" :items="['bread', 'coffee', 'fuel']" />
+        </div>
         <ExpenseItem v-for="item of paginatedExpenseItems" :expense="item" />
         <v-pagination
           v-model="page"
